149ec4240e1aeeb7a710f0769fbd8c6ae17aca06
Rithk <rithkchaudhary86@gmail.com>
2024-06-11 16:23:22 +0530

    monorepo building

---
+++ myturborepo/apps/web/app/admin/page.tsx
@@ -0,0 +1,8 @@
+import { Admin } from "@repo/ui/admin";
+
+export default function(){
+    return <div>
+        hi this is from the admin Page
+        <Admin />
+    </div>
+}
---
+++ myturborepo/apps/web/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@repo/ui/button";
+
+export default function Page(): JSX.Element{
+  return (
+    <div>
+      <Button appName="webapp"/> 
+      hi there
+    </div>
+  )
+}
